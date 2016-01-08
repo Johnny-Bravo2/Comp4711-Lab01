dd2f391150a6730246146fe4ca18a5735d6091fb
John Janzen <joalja2002@msn.com>
2016-01-07 16:55:57 -0800

    Operator Operator
    Some practice things

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <body>
         <?php
         echo 'hi there';
+        $name = 'John';
+        $what = 'nerrrrrrdddd';
+        $level = '9001';
+        echo 'hey, my name is ' .$name, '. and I am a level ' .$level, ' ' .$what;
         ?>
     </body>
 </html>
